cf95982062d0126b1fdb7ac1d4bce10d44aae88e
Jonathan Vice <jajvice@gmail.com>
2021-07-09 16:30:48 +0200

    Initial commit

---
+++ python/django-tut/mysite/polls/views.py
@@ -0,0 +1,64 @@
+from typing import Union
+
+from django.http import HttpResponse, HttpRequest
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """Excludes any questions that aren't published yet."""
+
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(
+    request: HttpRequest, question_id: int
+) -> Union[HttpResponse, HttpResponseRedirect]:
+    question = get_object_or_404(Question, pk=question_id)
+    selected_choice: Choice
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        print(question.choice_set.all())
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        # Always send a redirect after dealing with POST data
+        # this prevents accidentally resubmitting the data
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
